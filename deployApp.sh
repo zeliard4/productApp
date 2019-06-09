c3442eba0e8b30fd4ba9990c315cb403712055ee
root <root@zeliard-one.europe-west1-b.c.zeliard-224912.internal>
2019-06-09 23:02:56 +0000

    deploy script fixed, finished

--- deployApp.sh
+++ deployApp.sh
@@ -5,10 +5,9 @@
 ## script to build war and deploy it to tomcat container
 #########################################################################################################
 echo -e "\e[32m\nDEPLOYING PRODUCT APP...\e[39m\n"
-cd grails-app
-source infrastructure/infrastructure
-cd infrastructure
-app-grails-war-build
+rm -rf build/libs/*
+source grails-app/infrastructure/infrastructure
+app-grails-war-deploy
 app-tomcat-build
 app-tomcat-run
 echo -e "\e[32m\nDEPLOYED!\e[39m\n"
