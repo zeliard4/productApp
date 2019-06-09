5f7d54229447c80db2486d4286f5e1917e2c7c4b
Francesc Sola <fsola@hellomedia.com>
2019-06-10 00:32:03 +0200

    all working

--- deployApp.sh
+++ deployApp.sh
@@ -5,8 +5,9 @@
 ## script to build war and deploy it to tomcat container
 #########################################################################################################
 echo -e "\e[32m\nDEPLOYING PRODUCT APP...\e[39m\n"
-cd grails-app/infrastructure
-source infrastructure
+cd grails-app
+source infrastructure/infrastructure
+cd infrastructure
 app-grails-war-build
 app-tomcat-build
 app-tomcat-run
